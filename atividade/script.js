b7917799c64cf95a62e33e6e3dfd925ec5f74527
Lucas Matheus <108732635+Lucas230806@users.noreply.github.com>
2023-06-14 12:03:51 -0300

    Add files via upload

---
+++ atividade/script.js
@@ -0,0 +1,9 @@
+const  url = "https://api.github.com/users/Lucas230806"
+const nome = "Lucas230806"
+const seguidores = "https://api.github.com/users/Lucas230806/followers"
+
+
+function newtab(){
+    window.open("https://avatars.githubusercontent.com/u/108732635?v=4",)
+    console.log()
+}
